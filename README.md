1986eb15a1cc8ab5f1856dbb343c20adefb6022d
Miguel Carpio <mcarpio@redhat.com>
2024-09-25 19:25:01 +0200

    p2 solucio

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TDIW
 Tecnologies de desenvolupament per a Internet i Web
+
+SESSIÓ DE PROBLEMES 2 – Documents web (CSS)
---
+++ js/funcions.js
@@ -0,0 +1,6 @@
+function confirmaRegistre(){
+    alert("registrant estudiant");
+    document.getElementById("formDiv").innerHTML = "<p class='important'>T'has registrat amb èxit!</p>";
+    console.log("registrant estudiant");
+    return false;
+}
